4c51d94add6fd607efb0ce70aae76dc7f8fe44c6
edwin <edwinswatpako@gmail.com>
2024-01-04 20:22:00 +0700

    chore: fmt applied

--- indexer/src/errors.rs
+++ indexer/src/errors.rs
@@ -5,7 +5,7 @@ pub enum Error {
     #[error(transparent)]
     IoError(#[from] std::io::Error),
     #[error("anyhow error")]
-    AnyhowError(#[from] anyhow::Error)
+    AnyhowError(#[from] anyhow::Error),
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
