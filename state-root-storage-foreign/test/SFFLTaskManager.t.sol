aa7d19710856aebf674873923d002c4b7060d688
Franco Barpp Gomes <franco@nethermind.io>
2024-01-12 01:00:24 -0300

    test: Add SFFLTaskManager unit tests

---
+++ state-root-storage-foreign/test/SFFLTaskManager.t.sol
@@ -0,0 +1,359 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+import {BLSMockAVSDeployer} from "eigenlayer-middleware/test/utils/BLSMockAVSDeployer.sol";
+import {BN254} from "eigenlayer-middleware/src/libraries/BN254.sol";
+import {IRegistryCoordinator} from "eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
+import {IBLSSignatureChecker} from "eigenlayer-middleware/src/interfaces/IBLSSignatureChecker.sol";
+
+import {SFFLTaskManager} from "../src/eth/SFFLTaskManager.sol";
+import {Checkpoint} from "../src/eth/task/Checkpoint.sol";
+
+import {TestUtils} from "./utils/TestUtils.sol";
+
+contract SFFLTaskManagerHarness is SFFLTaskManager {
+    constructor(IRegistryCoordinator registryCoordinator, uint32 taskResponseWindowBlock)
+        SFFLTaskManager(registryCoordinator, taskResponseWindowBlock)
+    {}
+}
+
+contract SFFLTaskManagerTest is TestUtils {
+    using Checkpoint for Checkpoint.Task;
+    using Checkpoint for Checkpoint.TaskResponse;
+    using BN254 for BN254.G1Point;
+
+    SFFLTaskManagerHarness public taskManager;
+
+    uint32 public constant TASK_RESPONSE_WINDOW_BLOCK = 30;
+    address public aggregator;
+    address public generator;
+    uint256 public thresholdDenominator;
+
+    event CheckpointTaskCreated(uint32 indexed taskIndex, Checkpoint.Task task);
+    event CheckpointTaskResponded(
+        Checkpoint.TaskResponse taskResponse, Checkpoint.TaskResponseMetadata taskResponseMetadata
+    );
+    event CheckpointTaskCompleted(uint32 indexed taskIndex);
+    event CheckpointTaskChallengedSuccessfully(uint32 indexed taskIndex, address indexed challenger);
+    event CheckpointTaskChallengedUnsuccessfully(uint32 indexed taskIndex, address indexed challenger);
+
+    function setUp() public {
+        _setUpBLSMockAVSDeployer();
+
+        aggregator = addr("aggregator");
+        generator = addr("generator");
+
+        address impl = address(new SFFLTaskManagerHarness(registryCoordinator, TASK_RESPONSE_WINDOW_BLOCK));
+
+        taskManager = SFFLTaskManagerHarness(
+            deployProxy(
+                impl,
+                address(proxyAdmin),
+                abi.encodeWithSelector(
+                    taskManager.initialize.selector, pauserRegistry, registryCoordinatorOwner, aggregator, generator
+                )
+            )
+        );
+
+        vm.label(impl, "taskManagerImpl");
+        vm.label(address(taskManager), "taskManagerProxy");
+
+        thresholdDenominator = taskManager.THRESHOLD_DENOMINATOR();
+    }
+
+    function test_createCheckpointTask_RevertWhen_CallerNotTaskGenerator() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 100,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        vm.expectRevert("Task generator must be the caller");
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+    }
+
+    function test_createCheckpointTask() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 100,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        assertEq(taskManager.latestCheckpointTaskNum(), 0);
+        assertEq(taskManager.allCheckpointTaskHashes(0), bytes32(0));
+
+        vm.roll(task.taskCreatedBlock);
+
+        vm.expectEmit(true, false, false, true);
+        emit CheckpointTaskCreated(0, task);
+
+        vm.prank(generator);
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+
+        assertEq(taskManager.latestCheckpointTaskNum(), 1);
+        assertEq(taskManager.allCheckpointTaskHashes(0), task.hash());
+    }
+
+    function test_respondToCheckpointTask() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 1000,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        Checkpoint.TaskResponse memory taskResponse = Checkpoint.TaskResponse({
+            referenceTaskIndex: 0,
+            stateRootUpdatesRoot: keccak256(hex"beef"),
+            operatorSetUpdatesRoot: keccak256(hex"f00d")
+        });
+
+        (
+            bytes32 signatoryRecordHash,
+            IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = setUpOperators(taskResponse.hash(), task.taskCreatedBlock, 100, 1);
+
+        Checkpoint.TaskResponseMetadata memory taskResponseMetadata = Checkpoint.TaskResponseMetadata({
+            taskRespondedBlock: task.taskCreatedBlock + TASK_RESPONSE_WINDOW_BLOCK,
+            hashOfNonSigners: signatoryRecordHash
+        });
+
+        vm.prank(generator);
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+
+        assertEq(taskManager.allCheckpointTaskResponses(0), bytes32(0));
+
+        vm.roll(taskResponseMetadata.taskRespondedBlock);
+
+        vm.expectEmit(false, false, false, true);
+        emit CheckpointTaskResponded(taskResponse, taskResponseMetadata);
+
+        vm.prank(aggregator);
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+
+        assertEq(taskManager.allCheckpointTaskResponses(0), taskResponse.hashAgreement(taskResponseMetadata));
+    }
+
+    function test_respondToCheckpointTask_RevertWhen_InvalidTaskHash() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 1000,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        Checkpoint.TaskResponse memory taskResponse = Checkpoint.TaskResponse({
+            referenceTaskIndex: 1,
+            stateRootUpdatesRoot: keccak256(hex"beef"),
+            operatorSetUpdatesRoot: keccak256(hex"f00d")
+        });
+
+        (
+            bytes32 signatoryRecordHash,
+            IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = setUpOperators(taskResponse.hash(), task.taskCreatedBlock, 100, 1);
+
+        Checkpoint.TaskResponseMetadata memory taskResponseMetadata = Checkpoint.TaskResponseMetadata({
+            taskRespondedBlock: task.taskCreatedBlock + TASK_RESPONSE_WINDOW_BLOCK,
+            hashOfNonSigners: signatoryRecordHash
+        });
+
+        vm.prank(generator);
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+
+        assertEq(taskManager.allCheckpointTaskResponses(0), bytes32(0));
+
+        vm.roll(taskResponseMetadata.taskRespondedBlock);
+
+        vm.expectRevert("Wrong task hash");
+
+        vm.prank(aggregator);
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+    }
+
+    function test_respondToCheckpointTask_RevertWhen_ReResponding() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 1000,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        Checkpoint.TaskResponse memory taskResponse = Checkpoint.TaskResponse({
+            referenceTaskIndex: 0,
+            stateRootUpdatesRoot: keccak256(hex"beef"),
+            operatorSetUpdatesRoot: keccak256(hex"f00d")
+        });
+
+        (
+            bytes32 signatoryRecordHash,
+            IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = setUpOperators(taskResponse.hash(), task.taskCreatedBlock, 100, 1);
+
+        Checkpoint.TaskResponseMetadata memory taskResponseMetadata = Checkpoint.TaskResponseMetadata({
+            taskRespondedBlock: task.taskCreatedBlock + TASK_RESPONSE_WINDOW_BLOCK,
+            hashOfNonSigners: signatoryRecordHash
+        });
+
+        vm.prank(generator);
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+
+        assertEq(taskManager.allCheckpointTaskResponses(0), bytes32(0));
+
+        vm.roll(taskResponseMetadata.taskRespondedBlock);
+
+        vm.prank(aggregator);
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+
+        vm.expectRevert("Task already responded");
+
+        vm.prank(aggregator);
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+    }
+
+    function test_respondToCheckpointTask_RevertWhen_ResponseTimeExceeded() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 1000,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        Checkpoint.TaskResponse memory taskResponse = Checkpoint.TaskResponse({
+            referenceTaskIndex: 0,
+            stateRootUpdatesRoot: keccak256(hex"beef"),
+            operatorSetUpdatesRoot: keccak256(hex"f00d")
+        });
+
+        (
+            bytes32 signatoryRecordHash,
+            IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = setUpOperators(taskResponse.hash(), task.taskCreatedBlock, 100, 1);
+
+        Checkpoint.TaskResponseMetadata memory taskResponseMetadata = Checkpoint.TaskResponseMetadata({
+            taskRespondedBlock: task.taskCreatedBlock + TASK_RESPONSE_WINDOW_BLOCK + 1,
+            hashOfNonSigners: signatoryRecordHash
+        });
+
+        vm.prank(generator);
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+
+        assertEq(taskManager.allCheckpointTaskResponses(0), bytes32(0));
+
+        vm.roll(taskResponseMetadata.taskRespondedBlock);
+
+        vm.expectRevert("Response time exceeded");
+
+        vm.prank(aggregator);
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+    }
+
+    function test_respondToCheckpointTask_RevertWhen_QuorumNotMet() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 1000,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1) + 1,
+            quorumNumbers: hex"00"
+        });
+
+        Checkpoint.TaskResponse memory taskResponse = Checkpoint.TaskResponse({
+            referenceTaskIndex: 0,
+            stateRootUpdatesRoot: keccak256(hex"beef"),
+            operatorSetUpdatesRoot: keccak256(hex"f00d")
+        });
+
+        (
+            bytes32 signatoryRecordHash,
+            IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = setUpOperators(taskResponse.hash(), task.taskCreatedBlock, 100, 1);
+
+        Checkpoint.TaskResponseMetadata memory taskResponseMetadata = Checkpoint.TaskResponseMetadata({
+            taskRespondedBlock: task.taskCreatedBlock + TASK_RESPONSE_WINDOW_BLOCK,
+            hashOfNonSigners: signatoryRecordHash
+        });
+
+        vm.prank(generator);
+        taskManager.createCheckpointTask(
+            task.fromNearBlock, task.toNearBlock, task.quorumThresholdPercentage, task.quorumNumbers
+        );
+
+        assertEq(taskManager.allCheckpointTaskResponses(0), bytes32(0));
+
+        vm.roll(taskResponseMetadata.taskRespondedBlock);
+
+        vm.expectRevert("Quorum percentage not met");
+
+        vm.prank(aggregator);
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+    }
+
+    function test_respondToCheckpointTask_RevertWhen_CallerNotAggregator() public {
+        Checkpoint.Task memory task = Checkpoint.Task({
+            taskCreatedBlock: 1000,
+            fromNearBlock: 0,
+            toNearBlock: 1,
+            quorumThresholdPercentage: quorumThreshold(thresholdDenominator, 1),
+            quorumNumbers: hex"00"
+        });
+
+        Checkpoint.TaskResponse memory taskResponse = Checkpoint.TaskResponse({
+            referenceTaskIndex: 0,
+            stateRootUpdatesRoot: keccak256(hex"beef"),
+            operatorSetUpdatesRoot: keccak256(hex"f00d")
+        });
+
+        IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature;
+
+        vm.expectRevert("Aggregator must be the caller");
+
+        taskManager.respondToCheckpointTask(task, taskResponse, nonSignerStakesAndSignature);
+    }
+
+    function test_checkQuorum() public {
+        uint32 blockNumber = 1000;
+        bytes32 _msgHash = keccak256("test");
+
+        (
+            bytes32 expectedSignatoryRecordHash,
+            IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        ) = setUpOperators(_msgHash, blockNumber, 100, 1);
+
+        vm.roll(blockNumber);
+
+        (bool success, bytes32 signatoryRecordHash) =
+            taskManager.checkQuorum(_msgHash, hex"00", blockNumber, nonSignerStakesAndSignature, 0);
+
+        assertTrue(success);
+        assertEq(signatoryRecordHash, expectedSignatoryRecordHash);
+
+        (success, signatoryRecordHash) = taskManager.checkQuorum(
+            _msgHash, hex"00", blockNumber, nonSignerStakesAndSignature, quorumThreshold(thresholdDenominator, 1) + 1
+        );
+
+        assertFalse(success);
+        assertEq(signatoryRecordHash, expectedSignatoryRecordHash);
+    }
+}
---
+++ state-root-storage-foreign/test/utils/TestUtils.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import {Test} from "forge-std/Test.sol";
+
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {BN254} from "eigenlayer-middleware/src/libraries/BN254.sol";
+import {BLSMockAVSDeployer} from "eigenlayer-middleware/test/utils/BLSMockAVSDeployer.sol";
+import {IBLSSignatureChecker} from "eigenlayer-middleware/src/interfaces/IBLSSignatureChecker.sol";
+
+abstract contract TestUtils is Test, BLSMockAVSDeployer {
+    using BN254 for BN254.G1Point;
+
+    function addr(string memory name) public returns (address) {
+        address resp = address(uint160(uint256(keccak256(abi.encodePacked(name)))));
+
+        vm.label(resp, name);
+
+        return resp;
+    }
+
+    function deployProxy(address implementation, address admin, bytes memory call) public returns (address) {
+        return address(new TransparentUpgradeableProxy(implementation, admin, call));
+    }
+
+    function setUpOperators(
+        bytes32 _msgHash,
+        uint32 taskCreationBlock,
+        uint256 pseudoRandomNumber,
+        uint256 numNonSigners
+    ) public returns (bytes32, IBLSSignatureChecker.NonSignerStakesAndSignature memory) {
+        msgHash = _msgHash;
+        _setAggregatePublicKeysAndSignature();
+
+        (, IBLSSignatureChecker.NonSignerStakesAndSignature memory nonSignerStakesAndSignature) =
+            _registerSignatoriesAndGetNonSignerStakeAndSignatureRandom(pseudoRandomNumber, numNonSigners, 1);
+
+        bytes32[] memory nonSignersPubkeyHashes = new bytes32[](nonSignerStakesAndSignature.nonSignerPubkeys.length);
+        for (uint256 i = 0; i < nonSignersPubkeyHashes.length; i++) {
+            nonSignersPubkeyHashes[i] = nonSignerStakesAndSignature.nonSignerPubkeys[i].hashG1Point();
+        }
+        bytes32 signatoryRecordHash = keccak256(abi.encodePacked(taskCreationBlock, nonSignersPubkeyHashes));
+
+        assertLe(block.number, taskCreationBlock);
+        vm.roll(taskCreationBlock);
+
+        return (signatoryRecordHash, nonSignerStakesAndSignature);
+    }
+
+    function quorumThreshold(uint256 denominator, uint256 nonSignerCount) public returns (uint32) {
+        return uint32((maxOperatorsToRegister - nonSignerCount) * denominator / maxOperatorsToRegister);
+    }
+}
