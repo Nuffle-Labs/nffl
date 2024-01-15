0af8e23632a09ef06e320fd5a99586d0c705eae4
Franco Barpp Gomes <franco@nethermind.io>
2024-01-15 16:51:54 -0300

    test: Update base registry tests and add updateAndGetStorageValue tests

--- state-root-storage-foreign/test/SFFLRegistryBase.t.sol
+++ state-root-storage-foreign/test/SFFLRegistryBase.t.sol
@@ -9,11 +9,21 @@ import {StateRootUpdate} from "../src/base/message/StateRootUpdate.sol";
 contract SFFLRegistryBaseHarness is SFFLRegistryBase {
     using StateRootUpdate for StateRootUpdate.Message;
 
+    bool public shouldDenyAgreement;
+
+    function setShouldDenyAgreement(bool _shouldDenyAgreement) external {
+        shouldDenyAgreement = _shouldDenyAgreement;
+    }
+
     function pushStateRoot(uint32 rollupId, uint64 blockHeight, bytes32 stateRoot) external {
         _pushStateRoot(rollupId, blockHeight, stateRoot);
     }
 
-    function _updateStateRoot(StateRootUpdate.Message calldata, bytes calldata) internal override {}
+    function _updateStateRoot(StateRootUpdate.Message calldata message, bytes calldata) internal override {
+        require(!shouldDenyAgreement, "Agreement denied");
+
+        _stateRootBuffers[message.rollupId][message.blockHeight] = message.stateRoot;
+    }
 }
 
 contract SFFLRegistryBaseTest is Test {
@@ -61,115 +71,150 @@ contract SFFLRegistryBaseTest is Test {
     }
 
     function test_getStorageValue() public {
-        address target = 0x0123456789012345678901234567890123456789;
-        bytes32 slot = 0xced0071642172bcb312d265ce65b397425c906a7575a4912408cf6c3a3265eb1;
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0x0123456789012345678901234567890123456789,
+            storageKey: 0xced0071642172bcb312d265ce65b397425c906a7575a4912408cf6c3a3265eb1,
+            stateTrieWitness: hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001"
+        });
+
         bytes32 stateRoot = 0x52fd73f9175ec160ff5fbf32a985447b4b95b87ac6b1860bf9a1fb81f954d774;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001";
         bytes32 expectedValue = 0x0000000000000000000000000000000000000000000000000000000000000001;
 
-        assertEq(registry.getStorageValue(target, slot, stateRoot, stateTrieWitness, storageTrieWitness), expectedValue);
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
+        registry.pushStateRoot(message.rollupId, message.blockHeight, message.stateRoot);
+
+        assertEq(registry.getStorageValue(message, proofParams), expectedValue);
     }
 
     function test_getStorageValue_RevertWhen_TargetDoesNotExist() public {
-        address target = 0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa;
-        bytes32 slot = 0xced0071642172bcb312d265ce65b397425c906a7575a4912408cf6c3a3265eb1;
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa,
+            storageKey: 0xced0071642172bcb312d265ce65b397425c906a7575a4912408cf6c3a3265eb1,
+            stateTrieWitness: hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001"
+        });
+
         bytes32 stateRoot = 0x52fd73f9175ec160ff5fbf32a985447b4b95b87ac6b1860bf9a1fb81f954d774;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001";
+
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
+        registry.pushStateRoot(message.rollupId, message.blockHeight, message.stateRoot);
 
         vm.expectRevert("Account does not exist");
-        registry.getStorageValue(target, slot, stateRoot, stateTrieWitness, storageTrieWitness);
+        registry.getStorageValue(message, proofParams);
     }
 
     function test_getStorageValue_RevertWhen_StorageValueDoesNotExist() public {
-        address target = 0x0123456789012345678901234567890123456789;
-        bytes32 slot = 0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0x0123456789012345678901234567890123456789,
+            storageKey: 0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+            stateTrieWitness: hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001"
+        });
+
         bytes32 stateRoot = 0x52fd73f9175ec160ff5fbf32a985447b4b95b87ac6b1860bf9a1fb81f954d774;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001";
+
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
+        registry.pushStateRoot(message.rollupId, message.blockHeight, message.stateRoot);
 
         vm.expectRevert("Storage value does not exist");
-        registry.getStorageValue(target, slot, stateRoot, stateTrieWitness, storageTrieWitness);
+        registry.getStorageValue(message, proofParams);
     }
 
-    function test_verifyStorage() public {
-        address target = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-        bytes32 slot = 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b;
+    function test_getStorageValue_RevertWhen_MismatchingStateRoots() public {
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0x0123456789012345678901234567890123456789,
+            storageKey: 0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb,
+            stateTrieWitness: hex"f86eb86cf86aa1200e61e9d7b7f8a76da4339d2962273cc6bee0df97274cb94e5b05588afe2b3a50b846f8448080a089cf4cf2ddd661535a4e28b774fec12c8fcb6ba78bc55946b00a9ab5e99c36e9a056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f83cb83af838a12008fcd933278b7ead42429ac19785950f9813a683a64c01a93405753905661aca95940000000000000000000000000000000000000001"
+        });
+
+        bytes32 stateRoot = 0x52fd73f9175ec160ff5fbf32a985447b4b95b87ac6b1860bf9a1fb81f954d774;
+
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
+
+        vm.expectRevert("Mismatching state roots");
+        registry.getStorageValue(message, proofParams);
+    }
+
+    function test_updateAndGetStorageValue_NoUpdate() public {
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+            storageKey: 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b,
+            stateTrieWitness: hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        });
+
         bytes32 stateRoot = 0xd223489d5fdd65f1fb9beb4dd16a35540d34993f68409f8033a27e8115020f15;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
         bytes32 expectedValue = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
-        registry.pushStateRoot(0, 0, stateRoot);
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
+        registry.pushStateRoot(message.rollupId, message.blockHeight, message.stateRoot);
 
-        assertTrue(
-            registry.verifyStorage(
-                0, 0, SFFLRegistryBase.ProofParams(target, slot, expectedValue, stateTrieWitness, storageTrieWitness)
-            )
-        );
+        registry.setShouldDenyAgreement(true);
+
+        assertEq(registry.getStateRoot(message.rollupId, message.blockHeight), stateRoot);
+
+        assertEq(registry.updateAndGetStorageValue(message, proofParams, hex""), expectedValue);
+
+        assertEq(registry.getStateRoot(message.rollupId, message.blockHeight), stateRoot);
     }
 
-    function test_verifyStorage_RevertWhen_TargetDoesNotExist() public {
-        address target = 0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa;
-        bytes32 slot = 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b;
+    function test_updateAndGetStorageValue_Update() public {
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+            storageKey: 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b,
+            stateTrieWitness: hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        });
+
         bytes32 stateRoot = 0xd223489d5fdd65f1fb9beb4dd16a35540d34993f68409f8033a27e8115020f15;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
         bytes32 expectedValue = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
-        registry.pushStateRoot(0, 0, stateRoot);
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
 
-        vm.expectRevert("Account does not exist");
-        registry.verifyStorage(
-            0, 0, SFFLRegistryBase.ProofParams(target, slot, expectedValue, stateTrieWitness, storageTrieWitness)
-        );
+        registry.setShouldDenyAgreement(false);
+
+        assertEq(registry.getStateRoot(message.rollupId, message.blockHeight), bytes32(0));
+
+        assertEq(registry.updateAndGetStorageValue(message, proofParams, hex"ff"), expectedValue);
+
+        assertEq(registry.getStateRoot(message.rollupId, message.blockHeight), stateRoot);
     }
 
-    function test_verifyStorage_RevertWhen_StorageValueDoesNotExist() public {
-        address target = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-        bytes32 slot = 0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
+    function test_updateAndGetStorageValue_RevertWhen_UpdateAndEmptyAgreement() public {
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+            storageKey: 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b,
+            stateTrieWitness: hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        });
+
         bytes32 stateRoot = 0xd223489d5fdd65f1fb9beb4dd16a35540d34993f68409f8033a27e8115020f15;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
-        bytes32 expectedValue = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
-        registry.pushStateRoot(0, 0, stateRoot);
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
 
-        vm.expectRevert("Storage value does not exist");
-        registry.verifyStorage(
-            0, 0, SFFLRegistryBase.ProofParams(target, slot, expectedValue, stateTrieWitness, storageTrieWitness)
-        );
+        registry.setShouldDenyAgreement(false);
+
+        vm.expectRevert("Empty agreement");
+        registry.updateAndGetStorageValue(message, proofParams, hex"");
     }
 
-    function test_verifyStorage_RevertWhen_StorageValueDoesNotMatchExpected() public {
-        address target = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-        bytes32 slot = 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b;
+    function test_updateAndGetStorageValue_RevertWhen_AgreementDenied() public {
+        SFFLRegistryBase.ProofParams memory proofParams = SFFLRegistryBase.ProofParams({
+            target: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+            storageKey: 0x470ebe1a3f7c174ece10a895b1c5597999ee280a8a7afaae776da0692fc28e7b,
+            stateTrieWitness: hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432",
+            storageTrieWitness: hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        });
+
         bytes32 stateRoot = 0xd223489d5fdd65f1fb9beb4dd16a35540d34993f68409f8033a27e8115020f15;
-        bytes memory stateTrieWitness =
-            hex"f86eb86cf86aa1209f74bd52020a869dbd6c5918e246e54fe47bed2b9e96439c406e5c0732d089bfb846f8448080a01efe59d3e576d132e64cb197fc20e8cb2ed260308c189f2a0bb14843eb126b1ca056570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432";
-        bytes memory storageTrieWitness =
-            hex"f848b846f844a1206cda01cb275318e1eb18c3a672f0185922be53b3d63d6a92fbf81420cf1a2783a1a0ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
-        bytes32 expectedValue = 0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
 
-        registry.pushStateRoot(0, 0, stateRoot);
+        StateRootUpdate.Message memory message = StateRootUpdate.Message(1, 2, stateRoot);
 
-        assertFalse(
-            registry.verifyStorage(
-                0, 0, SFFLRegistryBase.ProofParams(target, slot, expectedValue, stateTrieWitness, storageTrieWitness)
-            )
-        );
+        registry.setShouldDenyAgreement(true);
+
+        vm.expectRevert("Agreement denied");
+        registry.updateAndGetStorageValue(message, proofParams, hex"ff");
     }
 }
