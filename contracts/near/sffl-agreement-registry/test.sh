bb88684f0ff25c95007cbe3468ccf1f2972d574a
Franco Barpp Gomes <franco@nethermind.io>
2024-01-22 15:43:48 -0300

    chore: Fix deploy and test scripts wasm file name

--- contracts/near/sffl-agreement-registry/test.sh
+++ contracts/near/sffl-agreement-registry/test.sh
@@ -6,4 +6,4 @@ cargo test
 # sandbox testing
 ./build.sh
 cd sandbox-rs
-cargo run --example sandbox "../target/wasm32-unknown-unknown/release/sffl-agreement-registry.wasm"
+cargo run --example sandbox "../target/wasm32-unknown-unknown/release/sffl_agreement_registry.wasm"
