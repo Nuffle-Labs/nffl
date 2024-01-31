d250ec171cd4083533d2b252f5bde087f3123648
Franco Barpp Gomes <franco@nethermind.io>
2024-01-31 15:10:31 -0300

    fix: Fix utils setup contracts path

--- tests/utils.go
+++ tests/utils.go
@@ -26,7 +26,7 @@ func StartAnvilChainAndDeployContracts() *exec.Cmd {
 		"--private-key", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
 		"--broadcast",
 	)
-	cmd.Dir = "./contracts"
+	cmd.Dir = "./contracts/evm"
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
