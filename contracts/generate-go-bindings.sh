0044393bdb7d1e1d195ccfc900049b8c2e324a41
Franco Barpp Gomes <franco@nethermind.io>
2024-01-30 21:16:39 -0300

    feat: Update bindings and add clean up to binding generation script

--- contracts/generate-go-bindings.sh
+++ contracts/generate-go-bindings.sh
@@ -10,13 +10,13 @@ function create_binding {
     solc_abi=$(cat ${contract_json} | jq -r '.abi')
     solc_bin=$(cat ${contract_json} | jq -r '.bytecode.object')
 
-    mkdir -p data
-    echo ${solc_abi} >data/tmp.abi
-    echo ${solc_bin} >data/tmp.bin
+    mkdir -p tmp
+    echo ${solc_abi} >tmp/tmp.abi
+    echo ${solc_bin} >tmp/tmp.bin
 
     rm -f $binding_dir/${contract}/binding.go
-    abigen --bin=data/tmp.bin --abi=data/tmp.abi --pkg=contract${contract} --out=$binding_dir/${contract}/binding.go
-    rm -rf ../data/tmp.abi ../data/tmp.bin
+    abigen --bin=tmp/tmp.bin --abi=tmp/tmp.abi --pkg=contract${contract} --out=$binding_dir/${contract}/binding.go
+    rm -rf tmp
 }
 
 rm -rf bindings/*
