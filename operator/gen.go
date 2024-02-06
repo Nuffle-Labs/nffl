b2d6eeab4e22a712cc91bba0d5b67b83aa220136
Franco Barpp Gomes <franco@nethermind.io>
2024-02-06 07:51:48 -0300

    test: Remove Consumerer mock generation

--- operator/gen.go
+++ operator/gen.go
@@ -1,4 +1,3 @@
 package operator
 
 //go:generate mockgen -destination=./mocks/rpc_client.go -package=mocks github.com/NethermindEth/near-sffl/operator AggregatorRpcClienter
-//go:generate mockgen -destination=./mocks/consumer.go -package=mocks github.com/NethermindEth/near-sffl/operator Consumerer
