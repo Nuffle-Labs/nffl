31d80d8caf499c15f60b98756c0df616f713ea4f
Franco Barpp Gomes <franco@nethermind.io>
2024-02-13 16:48:59 -0300

    test: Refactor integration test

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -5,78 +5,139 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
 	"time"
 
-	"github.com/Layr-Labs/eigensdk-go/chainio/clients"
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
 	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	sdklogging "github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/Layr-Labs/eigensdk-go/signerv2"
 	sdkutils "github.com/Layr-Labs/eigensdk-go/utils"
-	"github.com/NethermindEth/near-sffl/aggregator"
-	"github.com/NethermindEth/near-sffl/core/chainio"
-	"github.com/NethermindEth/near-sffl/core/config"
-	"github.com/NethermindEth/near-sffl/operator"
-	"github.com/NethermindEth/near-sffl/types"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/NethermindEth/near-sffl/aggregator"
+	"github.com/NethermindEth/near-sffl/core/config"
+	"github.com/NethermindEth/near-sffl/operator"
+	"github.com/NethermindEth/near-sffl/types"
 )
 
-type IntegrationClients struct {
-	Sdkclients clients.Clients
-}
-
 func TestIntegration(t *testing.T) {
-	log.Println("This test takes ~50 seconds to run...")
+	t.Log("This test takes ~100 seconds to run...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
+	containersCtx, cancelContainersCtx := context.WithCancel(context.Background())
 
-	/* Start the anvil chain */
-	anvilC := startAnvilTestContainer(ctx, "8545")
-	// Not sure why but deferring anvilC.Terminate() causes a panic when the test finishes...
-	// so letting it terminate silently for now
-	anvilEndpoint, err := anvilC.Endpoint(ctx, "")
-	if err != nil {
-		t.Error(err)
-	}
-
-	anvilRollupC := startAnvilTestContainer(ctx, "8547")
-	anvilRollupEndpoint, err := anvilRollupC.Endpoint(ctx, "")
-	if err != nil {
-		t.Error(err)
-	}
+	mainnetAnvil := startAnvilTestContainer(t, containersCtx, "8545")
+	rollupAnvil := startAnvilTestContainer(t, containersCtx, "8547")
+	rollupAnvil := startAnvilTestContainer(t, ctx, "8547")
 
 	time.Sleep(4 * time.Second)
 
-	var aggConfigRaw config.ConfigRaw
-	aggConfigFilePath := "../../config-files/aggregator.yaml"
-	sdkutils.ReadYamlConfig(aggConfigFilePath, &aggConfigRaw)
-	aggConfigRaw.EthRpcUrl = "http://" + anvilEndpoint
-	aggConfigRaw.EthWsUrl = "ws://" + anvilEndpoint
+	sfflDeploymentRaw := readSfflDeploymentRaw()
 
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	nodeConfig := buildOperatorConfig(t, ctx, mainnetAnvil, rollupAnvil, rabbitMq)
+	config := buildAggregatorConfig(t, sfflDeploymentRaw, mainnetAnvil)
+
+	_ = startOperator(t, ctx, nodeConfig)
+	_ = startAggregator(t, ctx, config)
+
+	t.Cleanup(func() {
+		cancel()
+
+		time.Sleep(5 * time.Second)
+
+		if err := mainnetAnvil.Container.Terminate(containersCtx); err != nil {
+			t.Fatalf("Error terminating container: %s", err.Error())
+		}
+		if err := rollupAnvil.Container.Terminate(containersCtx); err != nil {
+			t.Fatalf("Error terminating container: %s", err.Error())
+		}
+		if err := rabbitMq.Terminate(containersCtx); err != nil {
+			t.Fatalf("Error terminating container: %s", err.Error())
+		}
+
+		cancelContainersCtx()
+	})
+}
+
+func startOperator(t *testing.T, ctx context.Context, nodeConfig types.NodeConfig) *operator.Operator {
+	operator, err := operator.NewOperatorFromConfig(nodeConfig)
+	if err != nil {
+		t.Fatalf("Failed to create operator: %s", err.Error())
+	}
+
+	go operator.Start(ctx)
+
+	t.Log("Started operator. Sleeping 15 seconds to give it time to register...")
+	time.Sleep(15 * time.Second)
+
+	return operator
+}
+
+func startAggregator(t *testing.T, ctx context.Context, config *config.Config) *aggregator.Aggregator {
+	t.Log("starting aggregator for integration tests")
+
+	agg, err := aggregator.NewAggregator(config)
+	if err != nil {
+		t.Fatalf("Failed to create aggregator: %s", err.Error())
+	}
+
+	go agg.Start(ctx)
+
+	t.Log("Started aggregator. Sleeping 20 seconds to give operator time to answer task 1...")
+	time.Sleep(20 * time.Second)
+
+	return agg
+}
+
+func readSfflDeploymentRaw() config.SFFLDeploymentRaw {
 	var sfflDeploymentRaw config.SFFLDeploymentRaw
 	sfflDeploymentFilePath := "../../contracts/evm/script/output/31337/sffl_avs_deployment_output.json"
 	sdkutils.ReadJsonConfig(sfflDeploymentFilePath, &sfflDeploymentRaw)
 
+	return sfflDeploymentRaw
+}
+
+func buildOperatorConfig(t *testing.T, mainnetAnvil, rollupAnvil *AnvilInstance) types.NodeConfig {
+	nodeConfig := types.NodeConfig{}
+	nodeConfigFilePath := "../../config-files/operator.anvil.yaml"
+	err := sdkutils.ReadYamlConfig(nodeConfigFilePath, &nodeConfig)
+	if err != nil {
+		t.Fatalf("Failed to read yaml config: %s", err.Error())
+	}
+
+	log.Println("starting operator for integration tests")
+	os.Setenv("OPERATOR_BLS_KEY_PASSWORD", "")
+	os.Setenv("OPERATOR_ECDSA_KEY_PASSWORD", "")
+	nodeConfig.BlsPrivateKeyStorePath = "../keys/test.bls.key.json"
+	nodeConfig.EcdsaPrivateKeyStorePath = "../keys/test.ecdsa.key.json"
+	nodeConfig.RegisterOperatorOnStartup = true
+	nodeConfig.EthRpcUrl = mainnetAnvil.HttpUrl
+	nodeConfig.EthWsUrl = mainnetAnvil.WsUrl
+	for id, _ := range nodeConfig.RollupIdsToRpcUrls {
+		nodeConfig.RollupIdsToRpcUrls[id] = mainnetAnvil.WsUrl
+	}
+
+	return nodeConfig
+}
+
+func buildAggregatorConfig(t *testing.T, sfflDeploymentRaw config.SFFLDeploymentRaw, mainnetAnvil *AnvilInstance) *config.Config {
+	var aggConfigRaw config.ConfigRaw
+	aggConfigFilePath := "../../config-files/aggregator.yaml"
+	sdkutils.ReadYamlConfig(aggConfigFilePath, &aggConfigRaw)
+	aggConfigRaw.EthRpcUrl = mainnetAnvil.HttpUrl
+	aggConfigRaw.EthWsUrl = mainnetAnvil.WsUrl
+
 	logger, err := sdklogging.NewZapLogger(aggConfigRaw.Environment)
 	if err != nil {
 		t.Fatalf("Failed to create logger: %s", err.Error())
-	}
-	ethRpcClient, err := eth.NewClient(aggConfigRaw.EthRpcUrl)
-	if err != nil {
-		t.Fatalf("Failed to create eth client: %s", err.Error())
-	}
-	ethWsClient, err := eth.NewClient(aggConfigRaw.EthWsUrl)
-	if err != nil {
-		t.Fatalf("Failed to create eth client: %s", err.Error())
 	}
 
 	aggregatorEcdsaPrivateKeyString := "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"
@@ -92,24 +153,19 @@ func TestIntegration(t *testing.T) {
 		t.Fatalf("Cannot get operator address: %s", err.Error())
 	}
 
-	chainId, err := ethRpcClient.ChainID(context.Background())
-	if err != nil {
-		t.Fatalf("Cannot get chainId: %s", err.Error())
-	}
-
-	privateKeySigner, _, err := signerv2.SignerFromConfig(signerv2.Config{PrivateKey: aggregatorEcdsaPrivateKey}, chainId)
+	privateKeySigner, _, err := signerv2.SignerFromConfig(signerv2.Config{PrivateKey: aggregatorEcdsaPrivateKey}, mainnetAnvil.ChainID)
 	if err != nil {
 		t.Fatalf("Cannot create signer: %s", err.Error())
 	}
-	txMgr := txmgr.NewSimpleTxManager(ethRpcClient, logger, privateKeySigner, aggregatorAddr)
+	txMgr := txmgr.NewSimpleTxManager(mainnetAnvil.HttpClient, logger, privateKeySigner, aggregatorAddr)
 
-	config := &config.Config{
+	return &config.Config{
 		EcdsaPrivateKey:                aggregatorEcdsaPrivateKey,
 		Logger:                         logger,
 		EthHttpRpcUrl:                  aggConfigRaw.EthRpcUrl,
-		EthHttpClient:                  ethRpcClient,
+		EthHttpClient:                  mainnetAnvil.HttpClient,
 		EthWsRpcUrl:                    aggConfigRaw.EthWsUrl,
-		EthWsClient:                    ethWsClient,
+		EthWsClient:                    mainnetAnvil.WsClient,
 		OperatorStateRetrieverAddr:     common.HexToAddress(sfflDeploymentRaw.Addresses.OperatorStateRetrieverAddr),
 		SFFLRegistryCoordinatorAddr:    common.HexToAddress(sfflDeploymentRaw.Addresses.RegistryCoordinatorAddr),
 		AggregatorServerIpPortAddr:     aggConfigRaw.AggregatorServerIpPortAddr,
@@ -119,86 +175,18 @@ func TestIntegration(t *testing.T) {
 		TxMgr:                          txMgr,
 		AggregatorAddress:              aggregatorAddr,
 	}
-
-	/* Prepare the config file for operator */
-	nodeConfig := types.NodeConfig{}
-	nodeConfigFilePath := "../../config-files/operator.anvil.yaml"
-	err = sdkutils.ReadYamlConfig(nodeConfigFilePath, &nodeConfig)
-	if err != nil {
-		t.Fatalf("Failed to read yaml config: %s", err.Error())
-	}
-	/* Register operator*/
-	// log.Println("registering operator for integration tests")
-	// we need to do this dynamically and can't just hardcode a registered operator into the anvil
-	// state because the anvil state dump doesn't also dump the receipts tree so we lose events,
-	// and the aggregator thus can't get the operator's pubkey
-	// operatorRegistrationCmd := exec.Command("bash", "./operator-registration.sh")
-	// err = operatorRegistrationCmd.Run()
-	// if err != nil {
-	// 	t.Fatalf("Failed to register operator: %s", err.Error())
-	// }
-
-	/* start operator */
-	// the passwords are set to empty strings
-	log.Println("starting operator for integration tests")
-	os.Setenv("OPERATOR_BLS_KEY_PASSWORD", "")
-	os.Setenv("OPERATOR_ECDSA_KEY_PASSWORD", "")
-	nodeConfig.BlsPrivateKeyStorePath = "../keys/test.bls.key.json"
-	nodeConfig.EcdsaPrivateKeyStorePath = "../keys/test.ecdsa.key.json"
-	nodeConfig.RegisterOperatorOnStartup = true
-	nodeConfig.EthRpcUrl = "http://" + anvilEndpoint
-	nodeConfig.EthWsUrl = "ws://" + anvilEndpoint
-	for id, _ := range nodeConfig.RollupIdsToRpcUrls {
-		nodeConfig.RollupIdsToRpcUrls[id] = "ws://" + anvilRollupEndpoint
-	}
-
-	operator, err := operator.NewOperatorFromConfig(nodeConfig)
-	if err != nil {
-		t.Fatalf("Failed to create operator: %s", err.Error())
-	}
-	go operator.Start(ctx)
-	log.Println("Started operator. Sleeping 15 seconds to give it time to register...")
-	time.Sleep(15 * time.Second)
-
-	/* start aggregator */
-	log.Println("starting aggregator for integration tests")
-	agg, err := aggregator.NewAggregator(config)
-	if err != nil {
-		t.Fatalf("Failed to create aggregator: %s", err.Error())
-	}
-	go agg.Start(ctx)
-	log.Println("Started aggregator. Sleeping 20 seconds to give operator time to answer task 1...")
-	time.Sleep(20 * time.Second)
-
-	// get avsRegistry client to interact with the chain
-	avsReader, err := chainio.BuildAvsReaderFromConfig(config)
-	if err != nil {
-		t.Fatalf("Cannot create AVS Reader: %s", err.Error())
-	}
-
-	// check if the task is recorded in the contract for task index 1
-	taskHash, err := avsReader.AvsServiceBindings.TaskManager.AllCheckpointTaskHashes(&bind.CallOpts{}, 1)
-	if err != nil {
-		t.Fatalf("Cannot get task hash: %s", err.Error())
-	}
-	if taskHash == [32]byte{} {
-		t.Fatalf("Task hash is empty")
-	}
-
-	// check if the task response is recorded in the contract for task index 1
-	taskResponseHash, err := avsReader.AvsServiceBindings.TaskManager.AllCheckpointTaskResponses(&bind.CallOpts{}, 1)
-	log.Printf("taskResponseHash: %v", taskResponseHash)
-	if err != nil {
-		t.Fatalf("Cannot get task response hash: %s", err.Error())
-	}
-	if taskResponseHash == [32]byte{} {
-		t.Fatalf("Task response hash is empty")
-	}
-
 }
 
-// TODO(samlaf): have to advance chain to a block where the task is answered
-func startAnvilTestContainer(ctx context.Context, exposedPort string) testcontainers.Container {
+type AnvilInstance struct {
+	Container  testcontainers.Container
+	HttpClient *eth.Client
+	HttpUrl    string
+	WsClient   *eth.Client
+	WsUrl      string
+	ChainID    *big.Int
+}
+
+func startAnvilTestContainer(t *testing.T, ctx context.Context, exposedPort string) *AnvilInstance {
 	integrationDir, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -224,15 +212,44 @@ func startAnvilTestContainer(ctx context.Context, exposedPort string) testcontai
 		Started:          true,
 	})
 	if err != nil {
-		panic(err)
+		t.Fatalf("Error starting anvil container: %s", err.Error())
 	}
 
-	advanceChain(anvilC)
+	advanceChain(t, anvilC)
 
-	return anvilC
+	anvilEndpoint, err := anvilC.Endpoint(ctx, "")
+	if err != nil {
+		t.Fatalf("Error getting anvil endpoint: %s", err.Error())
+	}
+
+	httpUrl := "http://" + anvilEndpoint
+	httpClient, err := eth.NewClient(httpUrl)
+	if err != nil {
+		t.Fatalf("Failed to create anvil HTTP client: %s", err.Error())
+	}
+
+	wsUrl := "ws://" + anvilEndpoint
+	wsClient, err := eth.NewClient(wsUrl)
+	if err != nil {
+		t.Fatalf("Failed to create anvil WS client: %s", err.Error())
+	}
+
+	chainId, err := httpClient.ChainID(ctx)
+	if err != nil {
+		t.Fatalf("Failed to get anvil chainId: %s", err.Error())
+	}
+
+	return &AnvilInstance{
+		Container:  anvilC,
+		HttpClient: httpClient,
+		HttpUrl:    httpUrl,
+		WsClient:   wsClient,
+		WsUrl:      wsUrl,
+		ChainID:    chainId,
+	}
 }
 
-func advanceChain(anvilC testcontainers.Container) {
+func advanceChain(t *testing.T, anvilC testcontainers.Container) {
 	anvilEndpoint, err := anvilC.Endpoint(context.Background(), "")
 	if err != nil {
 		panic(err)
@@ -246,14 +263,11 @@ func advanceChain(anvilC testcontainers.Container) {
 	)
 	cmd.Dir = "../../contracts/evm"
 
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = &stdout
+	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 	err = cmd.Run()
 
 	if err != nil {
-		fmt.Println(stderr.String())
-		panic(err)
+		t.Fatalf("Error advancing chain: %s", stderr.String())
 	}
-	fmt.Println(stdout.String())
 }
