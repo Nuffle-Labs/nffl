d289f8d679ea52e64dfb6ad6a3e51adf9d42b5e0
Franco Barpp Gomes <franco@nethermind.io>
2024-02-13 21:30:06 -0300

    feat: Move operator set update tracking to its own registry

--- contracts/evm/src/eth/SFFLRegistryCoordinator.sol
+++ contracts/evm/src/eth/SFFLRegistryCoordinator.sol
@@ -1,50 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.12;
 
+import {IPauserRegistry} from "eigenlayer-contracts/src/contracts/interfaces/IPauserRegistry.sol";
 import {IBLSApkRegistry} from "eigenlayer-middleware/src/interfaces/IBLSApkRegistry.sol";
 import {IStakeRegistry} from "eigenlayer-middleware/src/interfaces/IStakeRegistry.sol";
 import {IIndexRegistry} from "eigenlayer-middleware/src/interfaces/IIndexRegistry.sol";
 import {IServiceManager} from "eigenlayer-middleware/src/interfaces/IServiceManager.sol";
-import {BN254} from "eigenlayer-middleware/src/libraries/BN254.sol";
 
 import {RegistryCoordinator} from "../external/RegistryCoordinator.sol";
-import {Operators as RollupOperators} from "../rollup/utils/Operators.sol";
+import {SFFLOperatorSetUpdateRegistry} from "./SFFLOperatorSetUpdateRegistry.sol";
 
 /**
  * @title SFFL AVS Registry Coordinator
  * @notice Coordinator for various registries in an AVS - in this case,
- * {StakeRegistry}, {BLSApkRegistry} and {IndexRegistry}.
+ * the base {StakeRegistry}, {BLSApkRegistry} and {IndexRegistry}, but also
+ * {SFFLOperatorSetUpdateRegistry}.
  * This contract's behavior is basically similar to a base RegistryCoordinator,
- * with mainly the addition of operator set change tracking in order to trigger
- * the SFFL AVS to update the rollups' operator set copies.
- * @dev Operator set updates are block-based changes in the operator set which
- * are used by the AVS operators in order to update rollups' operator sets
- * (see {SFFLRegistryRollup}) through an {OperatorSetUpdate.Message}
- * attestation.
- * An operator set update is comprised of all the updates in operator weights
- * in one block, and as such happens at most once a block. It also has an
- * incrementing ID - which is then used on {OperatorSetUpdate.Message} and
- * could be used to fetch the update content for verifying evidences on bad
- * messages.
+ * with mainly the addition of operator set change tracking through
+ * {SFFLOperatorSetUpdateRegistry} in order to trigger the SFFL AVS to update
+ * the rollups' operator set copies.
  */
 contract SFFLRegistryCoordinator is RegistryCoordinator {
-    /**
-     * @notice Reference block numbers for each operator set update.
-     */
-    uint32[] public operatorSetUpdateIdToBlockNumber;
-
-    /**
-     * @notice Emitted when an operator set update is registered
-     * @param id Operator set update ID
-     */
-    event OperatorSetUpdatedAtBlock(uint64 indexed id);
+    SFFLOperatorSetUpdateRegistry public immutable operatorSetUpdateRegistry;
 
     constructor(
         IServiceManager _serviceManager,
         IStakeRegistry _stakeRegistry,
         IBLSApkRegistry _blsApkRegistry,
-        IIndexRegistry _indexRegistry
-    ) RegistryCoordinator(_serviceManager, _stakeRegistry, _blsApkRegistry, _indexRegistry) {}
+        IIndexRegistry _indexRegistry,
+        SFFLOperatorSetUpdateRegistry _operatorSetUpdateRegistry
+    ) RegistryCoordinator(_serviceManager, _stakeRegistry, _blsApkRegistry, _indexRegistry) {
+        operatorSetUpdateRegistry = _operatorSetUpdateRegistry;
+    }
+
+    // TODO: Include operator set update registry in registries. Currently
+    // RegistryCoordinator code size is already tight enough for any
+    // extensions.
+
+    // /**
+    //  * @inheritdoc RegistryCoordinator
+    //  */
+    // function initialize(
+    //     address _initialOwner,
+    //     address _churnApprover,
+    //     address _ejector,
+    //     IPauserRegistry _pauserRegistry,
+    //     uint256 _initialPausedStatus,
+    //     OperatorSetParam[] memory _operatorSetParams,
+    //     uint96[] memory _minimumStakes,
+    //     IStakeRegistry.StrategyParams[][] memory _strategyParams
+    // ) public override initializer {
+    //     RegistryCoordinator.initialize(
+    //         _initialOwner,
+    //         _churnApprover,
+    //         _ejector,
+    //         _pauserRegistry,
+    //         _initialPausedStatus,
+    //         _operatorSetParams,
+    //         _minimumStakes,
+    //         _strategyParams
+    //     );
+
+    //     registries.push(address(operatorSetUpdateRegistry));
+    // }
 
     /**
      * @inheritdoc RegistryCoordinator
@@ -112,101 +130,10 @@ contract SFFLRegistryCoordinator is RegistryCoordinator {
     }
 
     /**
-     * @notice Gets the previous and next operator sets for an operator set
-     * update. This should be used by AVS operators to agree on operator set
-     * updates to be pushed to rollups.
-     * Important: this assumes the AVS has only a #0 quorum.
-     * @dev This method's gas usage is high, and is meant for external calls,
-     * not transactions.
-     * @param operatorSetUpdateId Operator set update ID. Refer to
-     * {SFFLRegistryCoordinator}
-     * @return previousOperatorSet Operator set in the previous update, or an
-     * empty set if operatorSetUpdateId is 0
-     * @return newOperatorSet Operator set in the update indicated by
-     * `operatorSetUpdateId`
-     */
-    function getOperatorSetUpdate(uint64 operatorSetUpdateId)
-        external
-        view
-        returns (
-            RollupOperators.Operator[] memory previousOperatorSet,
-            RollupOperators.Operator[] memory newOperatorSet
-        )
-    {
-        IStakeRegistry _stakeRegistry = stakeRegistry;
-        IIndexRegistry _indexRegistry = indexRegistry;
-        IBLSApkRegistry _blsApkRegistry = blsApkRegistry;
-
-        if (operatorSetUpdateId > 0) {
-            previousOperatorSet = _getOperatorSetAtBlock(
-                operatorSetUpdateIdToBlockNumber[operatorSetUpdateId - 1],
-                _stakeRegistry,
-                _indexRegistry,
-                _blsApkRegistry
-            );
-        }
-
-        newOperatorSet = _getOperatorSetAtBlock(
-            operatorSetUpdateIdToBlockNumber[operatorSetUpdateId], _stakeRegistry, _indexRegistry, _blsApkRegistry
-        );
-    }
-
-    /**
-     * @notice Gets the count of how many operator set updates have happened
-     * to date, which is also the ID of the next update.
-     * @return Operator set update count
-     */
-    function getOperatorSetUpdateCount() external view returns (uint64) {
-        return uint64(operatorSetUpdateIdToBlockNumber.length);
-    }
-
-    /**
-     * @dev Gets the AVS operator set at a block number/height.
-     * Important: This assumes the AVS has only a #0 quorum. This method's gas
-     * usage is high, and is meant for usage in external calls, not
-     * transactions.
-     * @param blockNumber Block number for which to fetch the operator set
-     * @param _stakeRegistry Address of the AVS's {IStakeRegistry}
-     * @param _indexRegistry Address of the AVS's {IIndexRegistry}
-     * @param _blsApkRegistry Address of the AVS's {IBlsApkRegistry}
-     * @return Operator set at the specified block number
-     */
-    function _getOperatorSetAtBlock(
-        uint32 blockNumber,
-        IStakeRegistry _stakeRegistry,
-        IIndexRegistry _indexRegistry,
-        IBLSApkRegistry _blsApkRegistry
-    ) internal view returns (RollupOperators.Operator[] memory) {
-        bytes32[] memory operatorIds = _indexRegistry.getOperatorListAtBlockNumber(0, blockNumber);
-        RollupOperators.Operator[] memory operators = new RollupOperators.Operator[](operatorIds.length);
-
-        for (uint256 i = 0; i < operatorIds.length; i++) {
-            bytes32 operatorId = operatorIds[i];
-
-            address operator = _blsApkRegistry.getOperatorFromPubkeyHash(operatorId);
-            (BN254.G1Point memory pubkey,) = _blsApkRegistry.getRegisteredPubkey(operator);
-            uint96 stake = _stakeRegistry.getStakeAtBlockNumber(operatorId, 0, blockNumber);
-
-            operators[i] = RollupOperators.Operator({pubkey: pubkey, weight: stake});
-        }
-
-        return operators;
-    }
-
-    /**
-     * @dev Records an operator set update if necessary, i.e., if no other
-     * update happened in the same block.
-     * Emits {OperatorSetUpdatedAtBlock}.
+     * @dev Notifies {SFFLOperatorSetUpdateRegistry} of an operator set update
+     * in this block.
      */
     function _recordOperatorSetUpdate() internal {
-        uint64 id = uint64(operatorSetUpdateIdToBlockNumber.length);
-
-        if (id > 0 && operatorSetUpdateIdToBlockNumber[id - 1] == block.number) {
-            return;
-        }
-
-        emit OperatorSetUpdatedAtBlock(id);
-
-        operatorSetUpdateIdToBlockNumber.push(uint32(block.number));
+        operatorSetUpdateRegistry.recordOperatorSetUpdate();
     }
 }
