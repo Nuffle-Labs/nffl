65243b3708eee7a6292040a5926f53bb1b890cb6
Franco Barpp Gomes <franco@nethermind.io>
2024-02-19 06:42:01 -0300

    refactor: Remove indexer all shards tracking from Dockerfile

--- indexer/entrypoint.sh
+++ indexer/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Initialize NEAR Lake to generate config and genesis
 /indexer-app/indexer init --chain-id localnet
-
-# Tweak nearcore config to track all shards
-tmp=$(mktemp)
-jq '.tracked_shards = [0]' /root/.near/config.json >"$tmp" && mv "$tmp" /root/.near/config.json
-
 /indexer-app/indexer run "$@"
