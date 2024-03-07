4f8286d8e33175756c5e1ce7033b40237ed5dad4
Franco Barpp Gomes <franco@nethermind.io>
2024-03-06 22:02:20 -0300

    chore: Set contract licenses to MIT

--- contracts/evm/src/base/SFFLRegistryBase.sol
+++ contracts/evm/src/base/SFFLRegistryBase.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
 import {Lib_AddressResolver} from "@eth-optimism/contracts/libraries/resolver/Lib_AddressResolver.sol";
