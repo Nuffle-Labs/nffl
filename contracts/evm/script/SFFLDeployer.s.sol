b77b08bfb89728949fdbe192aec23a327647c869
Franco Barpp Gomes <franco@nethermind.io>
2024-03-07 10:24:01 -0300

    chore: Set contract licenses to MIT (#40)

--- contracts/evm/script/SFFLDeployer.s.sol
+++ contracts/evm/script/SFFLDeployer.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import {ProxyAdmin, TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
--- contracts/evm/src/eth/SFFLServiceManager.sol
+++ contracts/evm/src/eth/SFFLServiceManager.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
 import {ServiceManagerBase} from "eigenlayer-middleware/src/ServiceManagerBase.sol";
