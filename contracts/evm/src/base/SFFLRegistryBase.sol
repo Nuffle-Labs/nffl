d2b5f979584dd34aae5353c672afcfd943c8ccb5
Franco Barpp Gomes <franco@nethermind.io>
2024-04-04 22:53:16 -0300

    feat: Add storage gap to SFFLRegistryBase

--- contracts/evm/src/base/SFFLRegistryBase.sol
+++ contracts/evm/src/base/SFFLRegistryBase.sol
@@ -165,4 +165,11 @@ abstract contract SFFLRegistryBase {
 
         emit StateRootUpdated(rollupId, blockHeight, stateRoot);
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
