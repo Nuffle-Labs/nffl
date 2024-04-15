c013eaade30439c3c1e691253cbeb665fedbc592
Franco Barpp Gomes <franco@nethermind.io>
2024-04-15 10:59:17 -0300

    refactor: Split RollupBroadcaster and RollupWriter into separate files

--- aggregator/rollup_broadcaster.go
+++ aggregator/rollup_broadcaster.go
@@ -3,11 +3,8 @@ package aggregator
 import (
 	"context"
 	"errors"
-	"sync"
 	"time"
 
-	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
-	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/Layr-Labs/eigensdk-go/signerv2"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -20,172 +17,10 @@ import (
 	"github.com/NethermindEth/near-sffl/core/types/messages"
 )
 
-const NUM_OF_RETRIES = 5
-const TX_RETRY_INTERVAL = time.Millisecond * 200
-const OPERATOR_SET_RETRY_INTERVAL = time.Millisecond * 500
-
-type RollupWriter struct {
-	txMgr                 txmgr.TxManager
-	client                eth.EthClient
-	sfflRegistryRollup    *registryrollup.ContractSFFLRegistryRollup
-	rollupId              uint32
-	operatorSetUpdateLock sync.Mutex
-
-	logger logging.Logger
-}
-
-func NewRollupWriter(
-	ctx context.Context,
-	rollupId uint32,
-	rollupInfo config.RollupInfo,
-	signerConfig signerv2.Config,
-	address common.Address,
-	logger logging.Logger,
-) (*RollupWriter, error) {
-	client, err := eth.NewClient(rollupInfo.RpcUrl)
-	if err != nil {
-		return nil, err
-	}
-
-	chainId, err := client.ChainID(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	signerV2, _, err := signerv2.SignerFromConfig(signerConfig, chainId)
-	if err != nil {
-		panic(err)
-	}
-	txMgr := txmgr.NewSimpleTxManager(client, logger, signerV2, address)
-
-	sfflRegistryRollup, err := registryrollup.NewContractSFFLRegistryRollup(rollupInfo.SFFLRegistryRollupAddr, client)
-	if err != nil {
-		return nil, err
-	}
-
-	writer := &RollupWriter{
-		txMgr:              txMgr,
-		client:             client,
-		sfflRegistryRollup: sfflRegistryRollup,
-		rollupId:           rollupId,
-		logger:             logger,
-	}
-
-	return writer, nil
-}
-
-func (w *RollupWriter) InitializeOperatorSet(ctx context.Context, operators []registryrollup.RollupOperatorsOperator, operatorSetUpdateId uint64) error {
-	w.operatorSetUpdateLock.Lock()
-	defer w.operatorSetUpdateLock.Unlock()
-
-	w.logger.Info("Initializing operator set")
-
-	operation := func() error {
-		txOpts, err := w.txMgr.GetNoSendTxOpts()
-		if err != nil {
-			w.logger.Error("Error getting tx opts", "err", err)
-			return err
-		}
-
-		tx, err := w.sfflRegistryRollup.SetInitialOperatorSet(txOpts, operators, operatorSetUpdateId+1)
-		if err != nil {
-			w.logger.Error("Error assembling SetInitialOperatorSet tx", "err", err)
-			return err
-		}
-
-		_, err = w.txMgr.Send(ctx, tx)
-		if err != nil {
-			w.logger.Error("Error sending SetInitialOperatorSet tx", "err", err)
-			return err
-		}
-
-		return nil
-	}
-
-	for i := 0; i < NUM_OF_RETRIES; i++ {
-		err := operation()
-		if err == nil {
-			w.logger.Info("Rollup Operator set initialized")
-
-			return nil
-		} else {
-			// TODO: return on some tx errors
-			w.logger.Warn("Sending SetInitialOperatorSet failed", "err", err)
-		}
-
-		select {
-		case <-ctx.Done():
-			w.logger.Info("Context canceled")
-			return ctx.Err()
-
-		case <-time.After(TX_RETRY_INTERVAL):
-			continue
-		}
-	}
-
-	return errors.New("failed to initialize operator set after retries")
-}
-
-func (w *RollupWriter) UpdateOperatorSet(ctx context.Context, message messages.OperatorSetUpdateMessage, signatureInfo registryrollup.RollupOperatorsSignatureInfo) error {
-	w.operatorSetUpdateLock.Lock()
-	defer w.operatorSetUpdateLock.Unlock()
-
-	operation := func() error {
-		txOpts, err := w.txMgr.GetNoSendTxOpts()
-		if err != nil {
-			w.logger.Error("Error getting tx opts", "err", err)
-			return err
-		}
-
-		nextOperatorUpdateId, err := w.sfflRegistryRollup.NextOperatorUpdateId(&bind.CallOpts{})
-		if err != nil {
-			w.logger.Error("Error fetching NextOperatorUpdateId", "err", err)
-			return err
-		}
-
-		// TODO: queue in case message.id > nextOperatorUpdateId
-		if message.Id != nextOperatorUpdateId {
-			w.logger.Warn("MessageId didn't match nextOperatorUpdateId", "id", message.Id, "nextOperatorUpdateId", nextOperatorUpdateId)
-			return nil
-		}
-
-		tx, err := w.sfflRegistryRollup.UpdateOperatorSet(txOpts, message.ToBinding(), signatureInfo)
-		if err != nil {
-			w.logger.Error("Error assembling UpdateOperatorSet tx", "err", err)
-			return err
-		}
-
-		_, err = w.txMgr.Send(ctx, tx)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	for i := 0; i < NUM_OF_RETRIES; i++ {
-		err := operation()
-		if err == nil {
-			w.logger.Info("Rollup Operator set updated")
-
-			return nil
-		} else {
-			// TODO: return on some tx errors
-			w.logger.Warn("Sending UpdateOperatorSet failed", "err", err)
-		}
-
-		select {
-		case <-ctx.Done():
-			w.logger.Info("Context canceled")
-			return ctx.Err()
-
-		case <-time.After(TX_RETRY_INTERVAL):
-			continue
-		}
-	}
-
-	return errors.New("failed to update operator set after retries")
-}
+const (
+	GET_OPERATOR_SET_RETRIES        = 5
+	GET_OPERATOR_SET_RETRY_INTERVAL = time.Millisecond * 500
+)
 
 type RollupBroadcasterer interface {
 	BroadcastOperatorSetUpdate(ctx context.Context, message messages.OperatorSetUpdateMessage, signatureInfo registryrollup.RollupOperatorsSignatureInfo)
@@ -336,7 +171,7 @@ func (b *RollupBroadcaster) GetErrorChan() <-chan error {
 }
 
 func (b *RollupBroadcaster) tryGetOperatorSetById(ctx context.Context, avsReader chainio.AvsReaderer, operatorSetUpdateId uint64) ([]opsetupdatereg.RollupOperatorsOperator, error) {
-	for i := 0; i < NUM_OF_RETRIES; i++ {
+	for i := 0; i < GET_OPERATOR_SET_RETRIES; i++ {
 		operators, err := avsReader.GetOperatorSetById(ctx, operatorSetUpdateId)
 
 		if err == nil {
@@ -350,7 +185,7 @@ func (b *RollupBroadcaster) tryGetOperatorSetById(ctx context.Context, avsReader
 			b.logger.Info("Context canceled")
 			return nil, errors.New("failed to fetch operator set early")
 
-		case <-time.After(OPERATOR_SET_RETRY_INTERVAL):
+		case <-time.After(GET_OPERATOR_SET_RETRY_INTERVAL):
 			continue
 		}
 	}
