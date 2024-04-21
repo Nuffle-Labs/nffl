190a7b82329fd92f559b66b0b78cdd7715e788c3
Franco Barpp Gomes <franco@nethermind.io>
2024-04-21 09:49:36 -0300

    feat: Filter rabbit_publisher and sffl_indexer target logs

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -82,8 +82,9 @@ fn main() -> Result<()> {
     // (sending telemetry and downloading genesis)
     openssl_probe::init_ssl_cert_env_vars();
     let env_filter = near_o11y::tracing_subscriber::EnvFilter::new(
-        "nearcore=info,publisher=info,indexer=info,candidates_validator=info,tokio_reactor=info,near=info,\
-         stats=info,telemetry=info,near-performance-metrics=info",
+        "nearcore=info,publisher=info,indexer=info,candidates_validator=info,\
+         tokio_reactor=info,near=info,stats=info,telemetry=info,\
+         near-performance-metrics=info",
     );
     let _subscriber = near_o11y::default_subscriber(env_filter, &Default::default()).global();
     let opts: Opts = Opts::parse();
