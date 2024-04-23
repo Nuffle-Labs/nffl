36f8861e3b9fb7ec4a0f077735ddc1cdfcfe228c
Franco Barpp Gomes <franco@nethermind.io>
2024-04-23 10:49:20 -0300

    Add additional gas to RespondToCheckpointTask transaction (#113)

--- core/chainio/avs_writer.go
+++ core/chainio/avs_writer.go
@@ -112,11 +112,21 @@ func (w *AvsWriter) SendAggregatedResponse(
 		w.logger.Errorf("Error getting tx opts")
 		return nil, err
 	}
+
 	tx, err := w.AvsContractBindings.TaskManager.RespondToCheckpointTask(txOpts, task, taskResponse.ToBinding(), aggregation.ExtractBindingMainnet())
 	if err != nil {
 		w.logger.Error("Error submitting SubmitTaskResponse tx while calling respondToTask", "err", err)
 		return nil, err
 	}
+
+	txOpts.GasLimit = tx.Gas() * 15 / 10
+
+	tx, err = w.AvsContractBindings.TaskManager.RespondToCheckpointTask(txOpts, task, taskResponse.ToBinding(), aggregation.ExtractBindingMainnet())
+	if err != nil {
+		w.logger.Error("Error submitting SubmitTaskResponse tx while calling respondToTask", "err", err)
+		return nil, err
+	}
+
 	receipt, err := w.TxMgr.Send(ctx, tx)
 	if err != nil {
 		w.logger.Errorf("Error submitting CreateCheckpointTask tx")
