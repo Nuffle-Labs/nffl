2820ff23c6ce11d81db1d4b5a4e191a3eacf9510
Franco Barpp Gomes <franco@nethermind.io>
2024-04-29 20:01:44 -0300

    refactor: Refactor onTick into cases

--- operator/rpc_client.go
+++ operator/rpc_client.go
@@ -125,27 +125,26 @@ func (c *AggregatorRpcClient) handleRpcError(err error) error {
 }
 
 func (c *AggregatorRpcClient) onTick() {
-	tickerC := c.resendTicker.C
 	for {
-		// TODO(edwin): handle closed chan
-		<-tickerC
+		select {
+		case <-c.resendTicker.C:
+			err := c.InitializeClientIfNotExist()
+			if err != nil {
+				c.logger.Error("Error initializing client", "err", err)
+				continue
+			}
 
-		err := c.InitializeClientIfNotExist()
-		if err != nil {
-			c.logger.Error("Error initializing client", "err", err)
-			continue
-		}
-
-		{
 			c.unsentMessagesLock.Lock()
 			if len(c.unsentMessages) == 0 {
 				c.unsentMessagesLock.Unlock()
 				continue
 			}
 			c.unsentMessagesLock.Unlock()
-		}
 
-		c.tryResendFromDeque()
+			c.tryResendFromDeque()
+		default:
+			continue
+		}
 	}
 }
 
