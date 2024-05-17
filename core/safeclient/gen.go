09e0ad3a2f4e0dd8700a71c65c25cf9de78f9eb7
Franco Barpp Gomes <franco@nethermind.io>
2024-05-16 22:09:37 -0300

    test: Add SafeEthClient unit test

--- core/safeclient/gen.go
+++ core/safeclient/gen.go
@@ -1,3 +1,5 @@
 package safeclient
 
 //go:generate mockgen -destination=./mocks/safe_client.go -package=mocks github.com/NethermindEth/near-sffl/core/safeclient SafeClient
+//go:generate mockgen -destination=./mocks/eth_client.go -package=mocks github.com/Layr-Labs/eigensdk-go/chainio/clients/eth Client
+//go:generate mockgen -destination=./mocks/subscription.go -package=mocks github.com/ethereum/go-ethereum Subscription
