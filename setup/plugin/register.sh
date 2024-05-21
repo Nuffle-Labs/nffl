cc8386ee6dba09d40ddc63543c250767adc5a1cf
Franco Barpp Gomes <franco@nethermind.io>
2024-05-21 12:29:26 -0300

    feat: Add plugin setup

--- setup/plugin/register.sh
+++ setup/plugin/register.sh
@@ -5,14 +5,10 @@ if [ ! -f .env ]; then
   exit 1
 fi
 
-source .env
-
 echo "Using operator plugin to opt-in into NEAR SFFL"
 
 docker run --rm \
   -v $(pwd):/near-sffl/ \
-  -e ECDSA_KEY_PASSWORD=$OPERATOR_ECDSA_KEY_PASSWORD \
-  -e BLS_KEY_PASSWORD=$OPERATOR_BLS_KEY_PASSWORD \
-  -e CONFIG=/app/config.yaml \
+  --env-file .env \
   --pull=always \
   ghcr.io/nethermindeth/near-sffl/operator-plugin:latest --config /near-sffl/config/operator.yaml --operation-type opt-in
