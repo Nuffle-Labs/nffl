8048b421e6495189c6642cabc149226582564fa0
Franco Barpp Gomes <franco@nethermind.io>
2024-05-24 17:30:52 -0300

    Standardize subscription error handling (#183)
    * fix: Standardize subscription error handling
    
    * feat: Log header sub error on Attestor

--- operator/attestor/attestor.go
+++ operator/attestor/attestor.go
@@ -198,8 +198,10 @@ func (attestor *Attestor) processMQBlocks(ctx context.Context) {
 func (attestor *Attestor) processRollupHeaders(rollupId uint32, headersC chan *ethtypes.Header, subscription ethereum.Subscription, ctx context.Context) {
 	for {
 		select {
-		case <-subscription.Err():
-			attestor.logger.Error("Header subscription error", "rollupId", rollupId)
+		case err := <-subscription.Err():
+			attestor.logger.Error("Header subscription error", "rollupId", rollupId, "err", err)
+			subscription.Unsubscribe()
+			close(headersC)
 			return
 		case header, ok := <-headersC:
 			if !ok {
