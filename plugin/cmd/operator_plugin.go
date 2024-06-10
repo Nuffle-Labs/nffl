382a73657f489a57266fc3a6a4920e917ecfbc84
Lautaro Emanuel <emlautarom1@gmail.com>
2024-06-10 13:00:24 -0300

    Use simplified constructors

--- plugin/cmd/operator_plugin.go
+++ plugin/cmd/operator_plugin.go
@@ -9,7 +9,6 @@ import (
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients/wallet"
 	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
-	chainioutils "github.com/Layr-Labs/eigensdk-go/chainio/utils"
 	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
 	sdkecdsa "github.com/Layr-Labs/eigensdk-go/crypto/ecdsa"
 	"github.com/Layr-Labs/eigensdk-go/logging"
@@ -115,25 +114,17 @@ func NewOperatorPluginFromCLIContext(ctx *cli.Context) (*CliOperatorPlugin, erro
 		return nil, err
 	}
 
-	avsRegistryContractBindings, err := chainioutils.NewAVSRegistryContractBindings(
-		avsRegistryCoordinatorAddress,
-		operatorStateRetrieverAddress,
-		ethHttpClient,
-		logger,
-	)
+	elChainReader, err := chainio.BuildElReader(avsRegistryCoordinatorAddress, operatorStateRetrieverAddress, ethHttpClient, logger)
 	if err != nil {
-		logger.Error("Cannot create AVSRegistryContractBindings", "err", err)
+		logger.Error("Failed to create ElChainReader", "err", err)
 		return nil, err
 	}
 
-	elContractBindings, err := chainio.NewEigenlayerContractBindingsFromContract(avsRegistryContractBindings, ethHttpClient, logger)
+	elChainWriter, err := chainio.BuildElWriter(avsRegistryCoordinatorAddress, operatorStateRetrieverAddress, elChainReader, txMgr, ethHttpClient, logger)
 	if err != nil {
-		logger.Fatal("Failed to create EigenlayerContractBindings", "err", err)
+		logger.Error("Failed to create ElChainWriter", "err", err)
 		return nil, err
 	}
-
-	elChainReader := chainio.NewELChainReaderFromContract(elContractBindings, ethHttpClient, logger)
-	elChainWriter := chainio.NewElChainWriterFromBindings(elContractBindings, elChainReader, ethHttpClient, txMgr, logger)
 
 	avsManager, err := operator.NewAvsManager(
 		&avsConfig,
