ef2f604f76fbb19d912a4a54594cdf2941c1e722
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-06-29 00:41:48 +0200

    State Root update script

--- example/scripts/updateStateRoot.js
+++ example/scripts/updateStateRoot.js
@@ -1,43 +1,43 @@
-
 const { ethers } = require('ethers');
 const {NFFLRegistryRollupABI} = require('./abi/NFFLRegistryRollup');
 const {arbContracts} = require('./contracts');
 const config = require('./config.json');
 const { secretSeedPhrase } = require('../secret/secret');
 const { hashG1Point } = require('./src/hashG1Point');
-/* 
- * Automatically updates the operator set on Arbitrum.
+/*
+ * Updates the state root on Arbitrum to the Optimism block state.
 */
-async function updateOperatorSet() {
+async function updateStateRoot() {
     // Init provider
     const arbProvider = new ethers.JsonRpcProvider(config.arbRpcUrl, config.arbNetworkId);
     // Init wallet
     const wallet = ethers.Wallet.fromPhrase(secretSeedPhrase);
     const account = wallet.connect(arbProvider);
     console.log('Wallet address:', await account.getAddress());
-    // Get next operator update id
+    // Get RegistryRollup contract
     const registryRollup = new ethers.Contract(arbContracts.addresses.sfflRegistryRollup, NFFLRegistryRollupABI, account);
-    const nextOperatorUpdateId = await registryRollup.nextOperatorUpdateId();
-    console.log('nextOperatorUpdateId',nextOperatorUpdateId);
     // Fetch data
-    console.log(`${config.aggregator}/aggregation/operator-set-update?id=${nextOperatorUpdateId}`);
-    const response = await fetch(`${config.aggregator}/aggregation/operator-set-update?id=${nextOperatorUpdateId}`);
+    const blockHeight = 13905480;
+    console.log(`${config.aggregator}/aggregation/state-root-update?rollupId=11155420&blockHeight=${blockHeight}`);
+    const response = await fetch(`${config.aggregator}/aggregation/state-root-update?rollupId=11155420&blockHeight=${blockHeight}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const respText = await response.text();
-    text = respText.replace(/"Weight":\s*(\d+)/g, '"Weight": "$1"');
-    data = JSON.parse(text);
-    const operators = data.Message.Operators.map(({ Pubkey, Weight }) => ({
-        pubkey: Pubkey,
-        weight: Weight
-    }));
+    const data = await response.json();
+    // Build message
+    const nearDaTransactionId = '0x'+Buffer.from(data.Message.NearDaTransactionId).toString('hex');
+    const nearDaCommitment = '0x'+Buffer.from(data.Message.NearDaCommitment).toString('hex');
+    const stateRoot = '0x'+Buffer.from(data.Message.StateRoot).toString('hex');
     const message = {
-        id: data.Message.Id,
-        timestamp: data.Message.Timestamp,
-        operators
-    }
-    console.log('New operators:',message.operators);
+        rollupId: data.Message.RollupId,
+        blockHeight: data.Message.BlockHeight,
+        timestamp:data.Message.Timestamp,
+        nearDaTransactionId,
+        nearDaCommitment,
+        stateRoot
+    };
+    console.log(message);
+
     // Sort non-signers keys
     const nonSignerPubkeys = data.Aggregation.NonSignersPubkeysG1;
     nonSignerPubkeys.sort((a, b) => {
@@ -54,15 +54,16 @@ async function updateOperatorSet() {
         },
         sigma: data.Aggregation.SignersAggSigG1.g1_point
     }
-    // Call contact
-    const tx = await registryRollup.updateOperatorSet(message,signatureInfo);
+    console.log(signatureInfo)
+
+    // Update state root
+    const tx = await registryRollup.updateStateRoot(message,signatureInfo);
     console.log('transaction:', tx);
     await tx.wait();
-    // Get next operator update id
-    console.log('new nextOperatorUpdateId',await registryRollup.nextOperatorUpdateId());
+    console.log('State root updated');
 }
 
-updateOperatorSet()
+updateStateRoot()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
