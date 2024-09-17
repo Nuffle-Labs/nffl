445dec493474c8c251bd887b9c0e2650ca82346c
Franco Barpp Gomes <franco@nethermind.io>
2024-09-17 20:51:57 -0300

    feat: Add syncing script

---
+++ setup/operator/scripts/sync.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if ! command -v rclone &> /dev/null; then
+    echo "rclone could not be found. Please install rclone and try again."
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+
+source ../.env
+
+# Steps from https://near-nodes.io/intro/node-data-snapshots
+
+mkdir -p ~/.config/rclone
+touch ~/.config/rclone/rclone.conf
+
+RCLONE_CONFIG="
+[near_cf]
+type = s3
+provider = AWS
+download_url = https://dcf58hz8pnro2.cloudfront.net/
+acl = public-read
+server_side_encryption = AES256
+region = ca-central-1"
+
+if ! grep -q "\[near_cf\]" ~/.config/rclone/rclone.conf; then
+    echo "No [near_cf] section found in rclone.conf. Adding it."
+    echo "$RCLONE_CONFIG" >> ~/.config/rclone/rclone.conf
+fi
+
+chain="testnet"
+kind="rpc"
+
+rclone copy --no-check-certificate near_cf://near-protocol-public/backups/${chain:?}/${kind:?}/latest ./
+latest=$(cat latest)
+
+rm latest
+
+echo "Syncing ${latest} to ${NEAR_HOME_DIR}/data"
+
+echo "Removing existing data and config.json"
+rm -rf "${NEAR_HOME_DIR}/data"
+rm -rf "${NEAR_HOME_DIR}/config.json"
+
+mkdir -p "${NEAR_HOME_DIR}/data"
+
+echo "Copying snapshot"
+rclone copy --no-check-certificate --progress --transfers=6 --checkers=6 \
+  "near_cf://near-protocol-public/backups/${chain:?}/${kind:?}/${latest:?}" \
+  "${NEAR_HOME_DIR}/data"
+
+echo "Copying complete"
