4e36b3235285154f92a261f700d89020f9c5ded6
Firat Sertgoz <f@nuff.tech>
2024-09-28 01:38:35 +0200

    types.rs

--- operator-rs/src/types.rs
+++ operator-rs/src/types.rs
@@ -1,18 +1,26 @@
-#[derive(Clone, Deserialize)]
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct NodeConfig {
+    /// Used to set the logger level (true = info, false = debug)
     pub production: bool,
+    pub operator_address: String,
+    pub operator_state_retriever_address: String,
+    pub avs_registry_coordinator_address: String,
+    pub token_strategy_addr: String,
     pub eth_rpc_url: String,
     pub eth_ws_url: String,
-    pub operator_address: String,
     pub bls_private_key_store_path: String,
     pub ecdsa_private_key_store_path: String,
-    pub avs_registry_coordinator_address: String,
-    pub operator_state_retriever_address: String,
     pub aggregator_server_ip_port_address: String,
-    pub enable_metrics: bool,
-    pub eigen_metrics_ip_port_address: Option<String>,
-    pub node_api_ip_port_address: Option<String>,
-    pub task_response_wait_ms: u64,
     pub register_operator_on_startup: bool,
-    pub token_strategy_addr: Option<String>,
+    pub eigen_metrics_ip_port_address: String,
+    pub enable_metrics: bool,
+    pub node_api_ip_port_address: String,
+    pub enable_node_api: bool,
+    pub near_da_indexer_rmq_ip_port_address: String,
+    pub near_da_indexer_rollup_ids: Vec<u32>,
+    pub rollup_ids_to_rpc_urls: HashMap<u32, String>,
+    pub task_response_wait_ms: u32,
 }
