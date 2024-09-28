ce3fce426ce8f5ff1516d9590c5103d0c072b57d
firatsertgoz <firatsertgoz@sabanciuniv.edu>
2024-09-28 12:56:28 +0200

    wip: add fastnear types

--- indexer/src/types.rs
+++ indexer/src/types.rs
@@ -2,6 +2,7 @@ use std::collections::VecDeque;
 use std::fmt::Formatter;
 use std::{fmt, sync};
 use tokio::sync::Mutex;
+use near_indexer::{near_primitives::{hash::CryptoHash, types::ShardId, views::{BlockView, ExecutionOutcomeWithIdView, ReceiptView, StateChangeWithCauseView}}, IndexerChunkView, StreamerMessage};
 
 pub(crate) type ProtectedQueue<T> = sync::Arc<Mutex<VecDeque<T>>>;
 
@@ -11,6 +12,28 @@ pub(crate) struct CandidateData {
     pub transaction: near_indexer::IndexerTransactionWithOutcome,
     pub payloads: Vec<Vec<u8>>,
 }
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct BlockWithTxHashes {
+    pub block: BlockView,
+    pub shards: Vec<IndexerShardWithTxHashes>,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct IndexerShardWithTxHashes {
+    pub shard_id: ShardId,
+    pub chunk: Option<IndexerChunkView>,
+    pub receipt_execution_outcomes: Vec<IndexerExecutionOutcomeWithReceiptAndTxHash>,
+    pub state_changes: Vec<StateChangeWithCauseView>,
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct IndexerExecutionOutcomeWithReceiptAndTxHash {
+    pub execution_outcome: ExecutionOutcomeWithIdView,
+    pub receipt: ReceiptView,
+    pub tx_hash: Option<CryptoHash>,
+}
+
 
 impl fmt::Display for CandidateData {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
