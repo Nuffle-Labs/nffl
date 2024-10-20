2a08d9da95d78c6d4b424a066c0fd4e9db918d82
Firat Sertgoz <f@nuff.tech>
2024-10-20 11:36:58 +0200

    wip: add comments for config validation

--- operator-rs/src/config.rs
+++ operator-rs/src/config.rs
@@ -2,6 +2,8 @@ use crate::NFFLNodeConfig;
 use std::path::PathBuf;
 use anyhow::Result;
 
+
+// TODO: Add checks on the validation of the config file
 pub fn load_config(path: PathBuf) -> Result<NFFLNodeConfig> {
     let config_str = std::fs::read_to_string(path)?;
     let config: NFFLNodeConfig = serde_yaml::from_str(&config_str)?;
