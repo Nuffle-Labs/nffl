119506078ac821d02fac3db4637615b2a827162f
Sasha Syrotenko <alex.syrotenko.official@gmail.com>
2024-10-28 12:21:28 +0200

    Review comments-2

--- workers/src/verifier.rs
+++ workers/src/verifier.rs
@@ -9,22 +9,11 @@ use std::time::Duration;
 use tracing::error;
 
 #[derive(Serialize, Deserialize, std::fmt::Debug)]
-pub(crate) struct ResponseWrapper {
-    pub message: Message,
-}
-
-#[derive(Serialize, Deserialize, std::fmt::Debug)]
 pub(crate) struct Message {
     #[serde(rename = "RollupId")]
     rollup_id: u32,
     #[serde(rename = "BlockHeight")]
     block_height: u64,
-    #[serde(rename = "Timestamp")]
-    timestamp: u64,
-    #[serde(rename = "NearDaTransactionId")]
-    near_da_transaction_id: Vec<u8>,
-    #[serde(rename = "NearDaCommitment")]
-    near_da_commitment: Vec<u8>,
     #[serde(rename = "StateRoot")]
     pub(crate) state_root: Vec<u8>,
 }
@@ -48,7 +37,7 @@ impl NFFLVerifier {
         let client = ClientBuilder::new().build()?;
         let url: Url = eth_l2_url.parse()?;
         let provider = ProviderBuilder::new().on_http(url);
-        let agg_http_addr = format!("{}/state-root", agg_url);
+        let agg_http_addr = format!("{}/aggregation/state-root-update", agg_url);
 
         Ok(NFFLVerifier {
             eth_l2_provider: provider,
@@ -71,13 +60,13 @@ impl NFFLVerifier {
             tokio::time::timeout(TIMEOUT, self.get_block_state_root(block_height)),
         ) {
             Ok((Ok(agg_response), Ok(block_state_root))) => {
-                let state_root_slice: &[u8] = agg_response.message.state_root.as_slice();
+                let state_root_slice: &[u8] = agg_response.state_root.as_slice();
                 let aggregator_state_root: B256 = B256::from_slice(state_root_slice);
-                if agg_response.message.block_height != block_height {
+                if agg_response.block_height != block_height {
                     error!(
                         "Block heights are not equal for comparing state roots.\
                      Aggregator height: {:?}, L2-provided height: {:?}",
-                        agg_response.message.block_height, block_height
+                        agg_response.block_height, block_height
                     );
                     return Ok(false);
                 }
@@ -99,15 +88,16 @@ impl NFFLVerifier {
     }
 
     /// Fetches the root state from the NFFL aggregator via HTTP.
-    pub(crate) async fn get_aggregator_root_state(&self, block_height: u64) -> eyre::Result<ResponseWrapper> {
+    pub(crate) async fn get_aggregator_root_state(&self, block_height: u64) -> eyre::Result<Message> {
         let params = [
             ("rollupId", &self.network_id),
             ("blockHeight", &block_height.to_string()),
         ];
         let url = Url::parse_with_params(&self.aggregator_http_address, &params)?;
         let response = self.http_client.get(url).send().await?;
-        let result = response.json::<ResponseWrapper>().await?;
-        Ok(result)
+        let mut result: serde_json::Value = response.json().await?;
+        
+        serde_json::from_value(result["Message"].take()).map_err(Into::into)
     }
 
     /// Fetches the block state root from the Ethereum L2 provider
@@ -123,7 +113,7 @@ impl NFFLVerifier {
 
 #[cfg(test)]
 mod tests {
-    use crate::verifier::{Message, NFFLVerifier, ResponseWrapper};
+    use crate::verifier::NFFLVerifier;
     use std::time::Duration;
     use wiremock::matchers::{method, path, query_param_contains};
     use wiremock::{Mock, MockServer, ResponseTemplate};
@@ -185,7 +175,7 @@ mod tests {
         let state_root_resp_res = verifier.get_aggregator_root_state(2).await;
         assert!(state_root_resp_res.is_ok());
 
-        let state_root = state_root_resp_res.unwrap().message.state_root;
+        let state_root = state_root_resp_res.unwrap().state_root;
         assert_eq!(
             state_root,
             vec![
@@ -228,23 +218,32 @@ mod tests {
             false => vec![0; 32],
         };
 
-        let state_root_message = Message {
-            rollup_id: 1,
-            block_height: 2,
-            timestamp: 3,
-            near_da_transaction_id: vec![4],
-            near_da_commitment: vec![5],
-            state_root: expected_state_root,
-        };
-
-        let state_root_resp = ResponseWrapper {
-            message: state_root_message,
-        };
-
         Mock::given(method("GET"))
-            .and(path("/state-root"))
+            .and(path("/aggregation/state-root-update"))
             .and(query_param_contains("blockHeight", "2"))
-            .respond_with(ResponseTemplate::new(200).set_body_json(state_root_resp))
+            .respond_with(ResponseTemplate::new(200).set_body_json(serde_json::json!(
+                            {
+              "Message" : {
+                "RollupId" : 1,
+                "BlockHeight" : 2,
+                "Timestamp" : 3,
+                "NearDaTransactionId" : [ 4 ],
+                "NearDaCommitment" : [ 5 ],
+                "StateRoot" : expected_state_root,
+              },
+              "Aggregation" : {
+                "EthBlockNumber" : 0,
+                "MessageDigest" : [ 141 ],
+                "NonSignersPubkeysG1" : null,
+                "QuorumApksG1" : null,
+                "SignersApkG2" : null,
+                "SignersAggSigG1" : null,
+                "NonSignerQuorumBitmapIndices" : null,
+                "QuorumApkIndices" : null,
+                "TotalStakeIndices" : null,
+                "NonSignerStakeIndices" : null
+              }
+            })))
             .mount(mock_server)
             .await;
 
