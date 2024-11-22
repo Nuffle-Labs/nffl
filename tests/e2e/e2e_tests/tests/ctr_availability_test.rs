a858060ca17bf946dda4e137d2a1e91223ee031e
Sasha Syrotenko <alex.syrotenko.official@gmail.com>
2024-11-22 16:25:57 +0200

    Introduce profile for integration tests

--- tests/e2e/e2e_tests/tests/ctr_availability_test.rs
+++ tests/e2e/e2e_tests/tests/ctr_availability_test.rs
@@ -13,63 +13,63 @@ mod containers;
 use testcontainers::runners::AsyncRunner;
 use crate::containers::*;
 
-#[cfg(test)]
+#[cfg(all(test, feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_rabbitmq_available() {
     let rabbitmq = rabbitmq();
     rabbitmq.start().await.expect("");
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_anvil_node_available() {
     let anvil_node = anvil_node();
     anvil_node.start().await.expect("");
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_anvil_node_setup_available() {
     let setup = anvil_node_setup();
     setup.start().await.expect("");
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_anvil_rollup_node_available() {
     let rollup_node = anvil_rollup_node(8546); // Example port number
     rollup_node.start().await.expect("");
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_near_da_deployer_available() {
     let deployer = near_da_deployer(3030); // Example indexer port number
     deployer.start().await.expect("");
 }
 
-#[cfg(all(test, target_arch = "x86_64"))]
+#[cfg(all(test, target_arch = "x86_64", feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_rollup_relayer_available() {
     let relayer = rollup_relayer(8546); // Example rollup node port number
     relayer.start().await.expect("");
 }
 
-#[cfg(all(test, target_arch = "x86_64"))]
+#[cfg(all(test, target_arch = "x86_64", feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_indexer_available() {
     let indexer = indexer();
     indexer.start().await.expect("");
 }
 
-#[cfg(all(test, target_arch = "x86_64"))]
+#[cfg(all(test, target_arch = "x86_64", feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_aggregator_available() {
     let aggregator = aggregator();
     aggregator.start().await.expect("");
 }
 
-#[cfg(all(test, target_arch = "x86_64"))]
+#[cfg(all(test, target_arch = "x86_64", feature = "it-tests"))]
 #[tokio::test]
 pub async fn check_operator_available() {
     let operator = operator("../../../config-files/operator1-docker-compose.anvil.yaml"); // Example config path
